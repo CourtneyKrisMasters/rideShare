554a33a86dca8255d3d06af9978d1a877627c463
Courtney Masters <courtneymasters@Courtneys-MacBook-Pro.local>
2020-05-01 10:56:09 -0500

    adding more missing files

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 //import models
 
-const { Driver } = require("./Driver.js");
-const { Location } = require("./Location.js");
-const { Passenger } = require("./Passenger.js");
-const { Ride } = require("./Ride.js");
-const { State } = require("./State.js");
-const { Vehicle } = require("./Vehicle.js");
-const { VehicleType } = require("./VehicleType.js");
+const { Driver } = require("./models/Driver.js");
+const { Location } = require("./models/Location.js");
+const { Passenger } = require("./models/Passenger.js");
+const { Ride } = require("./models/Ride.js");
+const { State } = require("./models/State.js");
+const { Vehicle } = require("./models/Vehicle.js");
+const { VehicleType } = require("./models/VehicleType.js");
 
 // Configure Knex.
 //We just chose to connect to Julia's personal database - let us know if this is not okay
@@ -20,16 +20,6 @@ const knex = require("knex")({
         },
     });
     
-<<<<<<< HEAD
-    // Configure Objection.
-    const { Model } = require("objection");
-    Model.knex(knex);
-
-    
-    // add and update a vehicle
-    
-=======
 // Configure Objection.
 const { Model } = require("objection");
 Model.knex(knex);
->>>>>>> 50d2e4aa286b7f2fb49f58f2f4f4b2b691707ee3
