9ecf03f451a583bbda885ea81d38a4f960de00d1
unknown <rmwtaylor22@gmail.com>
2020-05-02 11:22:15 -0400

    updated

--- src/pages/Vehicles.vue
+++ src/pages/Vehicles.vue
@@ -2,8 +2,195 @@
   <v-container>
     <div>
       <h4 class="display-1">Welcome, Single-Page App User</h4>
+        <p class="body-1">This is the vehicle page.</p>
+        <h4 class="display-1">Add a vehicle</h4> <!-- make, model, color, vehicleTypeId(int), capacity(int), mpg(float), license state, liscence number -->
+          <v-form v-model="valid">
+                <v-text-field
+                v-model="memberResetInfo.make"  
+                v-bind:rules="rules.make"
+                error-count="10"
+                type="make"
+                label="Make"
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.model"
+                v-bind:rules="rules.model"
+                error-count="10"
+                type="model"
+                label="Model"
+                required
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.color"
+                v-bind:rules="rules.color"
+                error-count="10"
+                type="color"
+                label="Color"
+                required
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.vehicleTypeId"
+                v-bind:rules="rules.vehicleTypeId"
+                error-count="10"
+                type="vehicleTypeId"
+                label="Vehicle Type ID"
+                required
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.capacity"  
+                v-bind:rules="rules.capacity"
+                error-count="10"
+                type="capacity"
+                label="How many people can it hold?"
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.mpg"
+                v-bind:rules="rules.mpg"
+                error-count="10"
+                type="mpg"
+                label="MPG"
+                required
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.licenseState"
+                v-bind:rules="rules.licenseState"
+                error-count="10"
+                type="licenseState"
+                label="License State"
+                required
+                ></v-text-field>
+                <v-text-field
+                v-model="memberResetInfo.licenseNumber"
+                v-bind:rules="rules.licenseNumber"
+                error-count="10"
+                type="licenseNumber"
+                label="License Number"
+                required
+                ></v-text-field>
+            <v-btn v-bind:disabled="!valid" v-on:click="handleReset"
+              >Add Vehicle
+            </v-btn>
+          </v-form>
+      <div class="text-xs-center">
+        <v-dialog v-model="dialogVisible" width="500">
+          <v-card>
+            <v-card-title primary-title>
+              {{ dialogHeader }}
+            </v-card-title>
 
-      <p class="body-1">This is the vehicle page.</p>
+            <v-card-text>
+              {{ dialogText }}
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text v-on:click="hideDialog">Okay</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
     </div>
   </v-container>
 </template>
+
+
+<script>
+import Instructions from "../components/Instructions.vue";
+
+export default {
+  name: "ResetPasswordPage",
+  components: {
+    Instructions, // Use the Instructions component we just imported
+  },
+  data: function () {
+    return {
+      valid: false, // Are all the fields in the form valid?
+
+      // Object to collect account data
+      memberResetInfo: {
+        email: "",
+        password: "",
+        newPassword: "",
+        confirmedPassword: "",
+      },
+
+      // Was the account reset successfully?
+      accountReset: false,
+
+      // Data to be displayed by the dialog.
+      dialogHeader: "<no dialogHeader>",
+      dialogText: "<no dialogText>",
+      dialogVisible: false,
+
+      // Validation rules for the form fields. This functionality is an extension
+      // that's part of the Vuetify package. Each rule is a list of functions
+      // (note the fat arrows). Vuetify invokes all functions in the list,
+      // passing it the content of the associated form field. Functions should
+      // return either true (the field passes that validation) or a string
+      // containing an error message indicating why the field doesn't pass validation.
+      rules: {
+        required: [(val) => val.length > 0 || "Required"],
+        email: [
+          (val) => /\w{3,}@\w{3,}(?:.\w{3,})+$/.test(val) || "Invalid e-mail",
+        ],
+        password: [
+          (val) => /[A-Z]/.test(val) || "Need upper case letter",
+          (val) => /[a-z]/.test(val) || "Need lower case letter",
+          (val) => /\d/.test(val) || "Need digit",
+          (val) => val.length >= 8 || "Minimum 8 characters",
+        ],
+        matchingPassword: [
+            (val) => val == this.memberResetInfo.newPassword || "Does not match",  //not sure if this is the correct code, Maybe try this.newPassword
+        ],
+      },
+    };
+  },
+  methods: {
+    // Invoked when the user clicks the 'Reset Password' button.
+    handleReset: function () {
+      // Haven't been successful yet.
+      this.accountReset = false;
+
+      // Patch the content of the form to the Hapi server.
+      this.$axios
+        .patch("/reset-password", {
+          //password: this.newPassword,
+          email: this.memberResetInfo.email,
+          password: this.memberResetInfo.password,
+          newPassword: this.memberResetInfo.newPassword
+        })
+        .then((result) => {
+          // Based on whether things worked or not, show the
+          // appropriate dialog.
+          if (result.data.ok) {
+            this.showDialog("Success", result.data.msge);
+            this.accountReset = true;
+          } else {
+            this.showDialog("Sorry", result.data.msge);
+          }
+        })
+        .catch((err) => this.showDialog("Failed", err));
+    },
+
+    // Helper method to display the dialog box with the appropriate content.
+    showDialog: function (header, text) {
+      this.dialogHeader = header;
+      this.dialogText = text;
+      this.dialogVisible = true;
+    },
+
+    // Invoked by the "Okay" button on the dialog; dismiss the dialog
+    // and navigate to the home page.
+    hideDialog: function () {
+      this.dialogVisible = false;
+      if (this.accountReset) {
+        // Only navigate away from the reset page if we were successful.
+        this.$router.push({ name: "home-page" });
+      }
+    },
+  },
+};
+</script>
+
