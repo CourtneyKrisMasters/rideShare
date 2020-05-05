9f864b57cefa3a5735244b794806f7a38d8943a5
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-05-05 09:47:50 -0400

    Minor tweaks

--- db.js
+++ db.js
@@ -47,55 +47,41 @@ async function init() {
       prettyPrint: true,
     },
   });
-}
 
-async function init() {
-  // Show routes at startup.
-  await server.register(require("blipp"));
-
-  // Output logging information.
-  await server.register({
-    plugin: require("hapi-pino"),
-    options: {
-      prettyPrint: true,
-    },
-  });
-
-// Configure routes.
-server.route([
-  {
-    method: "GET",
-    path: "/vehicleTypes",
-    config: {
-      description: "Retrieve all vehicle types",
-    },
-    handler: async (request, h) => {
-      const vehicletypesInfo = await VehicleType.query()
-      .select()
-      return vehicletypesInfo;
-    }
-  },
-  
+  // Configure routes.
+  server.route([
     {
-        method: "GET",
-        path: "/admin",
-        config: {
-          description: "Retrieve all current rides",
-        },
-        handler: async (request, h) => {
-          const rideInfo = await Ride.query()
+      method: "GET",
+      path: "/vehicleTypes",
+      config: {
+        description: "Retrieve all vehicle types",
+      },
+      handler: async (request, h) => {
+        return VehicleType.query();
+      },
+    },
+
+    {
+      method: "GET",
+      path: "/admin",
+      config: {
+        description: "Retrieve all current rides",
+      },
+      handler: async (request, h) => {
+        const rideInfo = await Ride.query()
           .select()
-          .withGraphFetched('fromlocations.states')
-          .withGraphFetched('tolocations.states')
-          .withGraphFetched('vehicles')
-          .withGraphFetched('passengers')
-          .withGraphFetched('drivers');
-          return rideInfo; 
-    }
-  },
-  {
-    //method to add a new vehicle type to the database
-    method: "POST",
+          .withGraphFetched("fromlocations.states")
+          .withGraphFetched("tolocations.states")
+          .withGraphFetched("vehicles")
+          .withGraphFetched("passengers")
+          .withGraphFetched("drivers");
+        return rideInfo;
+      },
+    },
+
+    {
+      //method to add a new vehicle type to the database
+      method: "POST",
       path: "/vehicleTypes",
       config: {
         description: "Add a new vehicle type",
@@ -106,45 +92,45 @@ server.route([
         },
       },
       handler: async (request, h) => {
-        try{
-        //check if there is already that type in the database
-         const existingType = await VehicleType.query()
-          .where("type", request.payload.vehicleType)
-          .first();
-        if (existingType) {
+        try {
+          //check if there is already that type in the database
+          const existingType = await VehicleType.query()
+            .where("type", request.payload.vehicleType)
+            .first();
+          if (existingType) {
+            return {
+              ok: false,
+              msge: `Vehicle type '${request.payload.vehicleType}' is already defined`,
+            };
+          }
+          //if there is not already that type in the database, add new type
+          const newType = await VehicleType.query().insert({
+            type: request.payload.vehicleType,
+          });
+          //show results
+          if (newType) {
+            return {
+              ok: true,
+              msge: `Created type '${request.payload.vehicleType}'`,
+            };
+          } else {
+            return {
+              ok: false,
+              msge: `Couldn't create vehicle type '${request.payload.vehicleType}'`,
+            };
+          }
+        } catch (e) {
           return {
             ok: false,
-            msge: `Vehicle type '${request.payload.vehicleType}' is already defined`,
+            msge:
+              `Couldn't create vehicle type '${request.payload.vehicleType}'` +
+              e.message,
           };
         }
-        //if there is not already that type in the database, add new type
-        const newType = await VehicleType.query().insert({
-          type: request.payload.vehicleType,
-        });
-        //show results
-        if (newType) {
-          return {
-            ok: true,
-            msge: `Created type '${request.payload.vehicleType}'`,
-          };
-        } else {
-          return {
-            ok: false,
-            msge: `Couldn't create vehicle type '${request.payload.vehicleType}'`,
-          };
-      }
-    } catch(e) {
-      return {
-        ok: false,
-        msge: `Couldn't create vehicle type '${request.payload.vehicleType}'` + e.message,
-    };
-    }
-  }
-  }
-
-
-])
-await server.start();
+      },
+    },
+  ]);
+  await server.start();
 }
 
 // Go!
