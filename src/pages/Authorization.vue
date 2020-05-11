fea3965dfc2173fb240b1d8130e6e10eb7a6148a
Courtney Masters <courtneymasters@Courtneys-MacBook-Pro.local>
2020-05-11 18:06:27 -0500

    fixed errors

--- src/pages/Authorization.vue
+++ src/pages/Authorization.vue
@@ -9,9 +9,10 @@
           </v-toolbar>
           <v-card-text>
             <v-form>
-              <v-row >
+              <v-row>
                 <v-col class="d-flex">
                   <v-select
+                    v-model="authorizationObject.driverId"
                     :items="driverItems"
                     item-text="name"
                     item-value="id"
@@ -19,9 +20,10 @@
                   ></v-select>
                 </v-col>
               </v-row>
-              <v-row >
+              <v-row>
                 <v-col class="d-flex">
                   <v-select
+                    v-model="authorizationObject.vehicleId"
                     :items="vehicleItems"
                     item-text="licensenumber"
                     item-value="id"
@@ -33,7 +35,8 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer />
-            <v-btn v-on:click="handleSubmit" color="primary">Authorize Driver</v-btn>
+            <v-btn v-on:click="handleSubmit" color="primary"
+            >Authorize Driver</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -41,86 +44,83 @@
 
     <div class="text-xs-center">
         <v-dialog v-model="dialogVisible" width="500">
-          <v-card>
-            <v-card-title primary-title>
-              {{ dialogHeader }}
-            </v-card-title>
+        <v-card>
+          <v-card-title primary-title>
+            {{ dialogHeader }}
+          </v-card-title>
 
             <v-card-text>
-              {{ dialogText }}
-            </v-card-text>
+            {{ dialogText }}
+          </v-card-text>
 
             <v-divider></v-divider>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" text v-on:click="hideDialog">Okay</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text v-on:click="hideDialog">Okay</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
 
   </v-container>
 </template>
 
 <script>
 export default {
-  data: function() {
+  name: "Authorization",
+  data: function () {
     return {
         authorizationObject: {
-            driverId: "",
-            vehicleId: "",
-        },
+        driverId: "",
+        vehicleId: "",
+      },
 
-        driverItems: [],
-        vehicleItems: [],
+    driverItems: [],
+    vehicleItems: [],
 
-        // Was an account created successfully?
-        newauthorizationObjectCreated: false,
+    // Was an account created successfully?
+    newauthorizationObjectCreated: false,
 
-        // Data to be displayed by the dialog.
-        dialogHeader: "<no dialogHeader>",
-        dialogText: "<no dialogText>",
-        dialogVisible: false,
+    // Data to be displayed by the dialog.
+    dialogHeader: "<no dialogHeader>",
+    dialogText: "<no dialogText>",
+    dialogVisible: false,
 
-        rules: {
-            required: [(val) => val.length > 0 || "Required"]
-        }
+    rules: {
+        required: [(val) => val.length > 0 || "Required"]
+    }
     };
   },
 
-  //gets all drivers for the dropdown
-  mounted: function() {
-       this.$axios.get("/drivers").then(response => {
-        this.driverItems = response.data.map(item => ({
-          id: item.id,
-          firstname: item.firstname,
-          lastname: item.lastname,
-          name: item.firstname + " " + item.lastname,
-        }));
-      });
-      this.$axios.get("/vehicles").then(response => {
-        this.vehicleItems = response.data.map(item => ({
-          id: item.id,
-          licensenumber: item.licensenumber,
-        }));
-      });
-  },
+//gets all drivers for the dropdown
+mounted: function() {
+    this.$axios.get("/drivers").then(response => {
+    this.driverItems = response.data.map(item => ({
+        id: item.id,
+        firstname: item.firstname,
+        lastname: item.lastname,
+        name: item.firstname + " " + item.lastname,
+    }));
+    });
+    this.$axios.get("/vehicles").then(response => {
+    this.vehicleItems = response.data.map(item => ({
+        id: item.id,
+        licensenumber: item.licensenumber,
+    }));
+    });
+},
 
   methods: {
       // Invoked when the user clicks the 'Sign Up' button.
     handleSubmit: function () {
       // Haven't been successful yet.
       this.newauthorizationObjectCreated = false;
-      console.log("driver id: " + this.driverId.id);
-      console.log("vehicle id: " + this.vehicleId.id);
-      console.log("object driver id: " + this.authorizationObject.driverId);
-      console.log("object vehicle id: " + this.authorizationObject.id);
-      this.$axios //this probably needs to be fixed 
+      console.log("object vehicle id: " + this.authorizationObject.vehicleId);
+      this.$axios 
         .post("/authorization", {
-          //TODO - send the id to the db
-          driverId: this.driverId, //this is wrong
-          vehicleId: this.vehicleId,
+          driverId: this.authorizationObject.driverId,
+          vehicleId: this.authorizationObject.vehicleId,
         })
         .then((result) => {
           // Based on whether things worked or not, show the
@@ -151,7 +151,7 @@ export default {
         // Only navigate away from the sign-up page if we were successful.
         this.$router.push({ name: "admin" });
       }
-    }
+    },
   },
 };
 </script>
